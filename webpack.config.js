396258a453e93a8ff0d354cfcd97ba097533069e
Volodymyr <dev.volodymyr@gmail.com>
2018-05-01 18:58:13 +0300

    Added webpack and babel config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}
